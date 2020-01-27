e0b0819fb89d8902fd0e92476a0415616de5f0b9
Pavel <p.tataurov92@gmail.co>
2020-01-27 17:56:37 +0300

    update

---
+++ src/views/components/product-d/product-d.js
@@ -0,0 +1,7 @@
+$(() => {
+  const $productD = $('.product-d')
+
+  const $subSliderList = $('.product-d__sub-slider-list', $productD)
+
+  const $subSliderItem = $('.product-d__sub-slider-item', $subSliderList)
+})
